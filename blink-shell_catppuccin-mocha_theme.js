88c92b0a26854535869741a5e0428ccb5f7fbd4f
Felipe Felix <felixti@live.com>
2026-03-28 15:50:58 -0300

    chore: blink shell (ipadOS) - catppuccin mocha theme

---
+++ blink-shell_catppuccin-mocha_theme.js
@@ -0,0 +1,23 @@
+t.prefs_.set('color-palette-overrides', [
+  '#45475a',  // black (Surface1)
+  '#f38ba8',  // red
+  '#a6e3a1',  // green
+  '#f9e2af',  // yellow
+  '#89b4fa',  // blue
+  '#f5c2e7',  // magenta (Pink)
+  '#94e2d5',  // cyan (Teal)
+  '#bac2de',  // white (Subtext1)
+  '#585b70',  // bright black (Surface2)
+  '#f38ba8',  // bright red
+  '#a6e3a1',  // bright green
+  '#f9e2af',  // bright yellow
+  '#89b4fa',  // bright blue
+  '#f5c2e7',  // bright magenta
+  '#94e2d5',  // bright cyan
+  '#a6adc8'   // bright white (Subtext0)
+]);
+
+t.prefs_.set('cursor-color', '#f5e0dc');  // Rosewater
+t.prefs_.set('foreground-color', '#cdd6f4');  // Text
+t.prefs_.set('background-color', '#1e1e2e');  // Base
+t.prefs_.set('selection-color', '#353749');  // Surface0
